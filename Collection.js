11cf339cc6c4d0b5c9b43b91176e45c21f3a73da
Pawel Szmyd <lurid.pszmyd@gmail.com>
2021-04-10 00:19:11 +0200

    używanie metody z modułu collection w index.js

--- Collection.js
+++ Collection.js
@@ -1,10 +1,43 @@
+const colletion1 = {
+  id: "rapcytaty",
+  sentences: [
+  {id: 1,
+  sentence: "rób swoje i nie patrz się na innych jak ci coś nie wyjdzie to nie szukaj winnych"},
+  {id: 2,
+  sentence: "tylko to zostaje mi to po każdej zawleczki nakrętki i kapsle"},
+  {id: 3,
+  sentence: "duchy mierzą mnie swoimi pustymi ślepami patrzymy na siebie wiec długimi minutami"}
+  ]
+};
+const colletion2 = {
+  id: "żyrcie",
+  sentences: [
+    {id: 1, sentence: "muszynianka"},
+    {id: 2, sentence: "banany"},
+    {id: 3, sentence: "orzechy nerkowca"}
+  ]
+};
+const colletion3 = {
+  id: "kawa",
+  sentences: [
+    {id: 1, sentence: "z mlekiem"},
+    {id: 2, sentence: "bez mleka"},
+  ]
+};
+
+const data = [colletion1, colletion2, colletion3]
+
 class Collection {
   constructor() {
     
   }
-  displayCollection(e) {
+  getCollection(e, name) {
     e.preventDefault()
-    console.log("wyświetl kolekcję")
+    const collectionChosen = data.find(collection => {
+      return collection.id == name;
+    })
+    console.log(e.target);
+    return collectionChosen;
   }
 }
 export default Collection;
--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 import Collection from './Collection.js'
 
-const collection = new Collection()
+const collections = new Collection()
 
-const colletion1 = [
-  {id: 1,
-  sentence: "rób swoje i nie patrz się na innych jak ci coś nie wyjdzie to nie szukaj winnych"},
-  {id: 2,
-  sentence: "tylko to zostaje mi to po każdej zawleczki nakrętki i kapsle"},
-  {id: 3,
-  sentence: "duchy mierzą mnie swoimi pustymi ślepami patrzymy na siebie wiec długimi minutami"}
-];
-const colletion2 = [
-  {id: 1, sentence: "muszynianka"},
-  {id: 2, sentence: "banany"},
-  {id: 3, sentence: "orzechy nerkowca"}
-];
 
 const form = document.querySelector('.collectionChoice__form')
 
-form.addEventListener('submit', collection.displayCollection)
+const name = "selected value"
+
+form.addEventListener('submit', (e) => collections.getCollection(e, "kawa"))
